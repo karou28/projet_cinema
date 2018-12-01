68d59f1f0016951cca76a39304545e718227cac0
karou28 <pakd2013@gmail.com>
2018-12-01 00:59:29 +0000

    Starting
    let's go ibouibro !
    
    Co-Authored-By: ibouibro <ibouibro@users.noreply.github.com>

---
+++ Wcf_Project_Cinema/Wcf_Project_Cinema/ActorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Wcf_Project_Cinema
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ActorService" à la fois dans le code et le fichier de configuration.
+    public class ActorService : IActorService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
